ada545889d6cc668bd033bfbf8b4dd3ade7081fe
Felipe Fernandez <FelipeFelizx@gmail.com>
2023-01-04 19:08:01 -0300

    fix: Css filter

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -1,67 +1,117 @@
-import '../Styles/Filters.css'
-import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai"
-import { useState } from "react"
+import "../Styles/Filters.css";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 export default function Filters({ showFilters, setShowFilters, setLocation, adults, setAdults, children, setChildren, location }) {
-    const [showLocation, setShowLocation] = useState(true)
+    const [showLocation, setShowLocation] = useState(true);
+
     return (
-        <>
-            {showFilters && (
-                <div className="filters">
-                                        <div>
+        <div>
+            {showFilters &&
+                    
+                    <div className={`filters ${showFilters ? "show" : ""}`}>
                         <div>
-                            <h2 onClick={() => setShowLocation(true)} >Location</h2>
-                            <span onClick={() => setShowLocation(true)} >{location ? `${location}` : "Add location"}</span>
-                        </div>
-
-                        <div style={{ display: showLocation && "block", visibility: !showLocation && "hidden" }} >
-                            <ul>
-                                <li onClick={() => setLocation("Santiago")} >Santiago , Chile</li>
-                                <li onClick={() => setLocation("Valparaiso")} >Valparaiso , Chile</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div>
-                        
-                        <div>
-                            <h2 onClick={() => setShowLocation(false)} >Guests</h2>
-                            <span onClick={() => setShowLocation(false)} >{adults + children === 0 ? "Add guests" : `${adults + children}`}</span>
-                        </div>
-
-                        <div style={{ display: !showLocation && "block", visibility: showLocation && "hidden" }} >
-                            <div>
-                                <h2>Adults</h2>
-                                <span>Ages 13 or above</span>
-                                <div>
-                                    <button onClick={() => adults > 0 && setAdults((prevAults) => prevAults - 1)} >-</button>
-                                    <span>{adults}</span>
-                                    <button onClick={() => setAdults((prevAults) => prevAults + 1)} >+</button>
-                                </div>
+                            <div onClick={() => setShowLocation(true)}>
+                                <h2 >Location</h2>
+                                <span>
+                                    {location ? `${location}` : "Add location"}
+                                </span>
                             </div>
-
-                            <div>
-                                <h2>Children</h2>
-                                <span>Ages 2-12</span>
-                                <div>
-                                    <button onClick={() => children > 0 && setChildren((prevChildren) => prevChildren - 1)} >-</button>
-                                    <span>{children}</span>
-                                    <button onClick={() => setChildren((prevChildren) => prevChildren + 1)} >+</button>
-                                </div>
+                            <div className={`filters__options ${showLocation ? "show" : ""}`}>
+                                <ul >
+                                    <li onClick={() => setLocation("Santiago")}>
+                                        Santiago, Chile
+                                    </li>
+                                    <li onClick={() => setLocation("Valparaiso")}>
+                                        Valparaiso, Chile
+                                    </li>
+                                    <li onClick={() => setLocation("Osorno")}>
+                                        Osorno, Chile
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                    </div>
 
-                    <div>
-                        <h2>Edit your search</h2>
+                        <div>
+                            <div onClick={() => setShowLocation(false)}>
+                                <h2 >Guests</h2>
+                                <span>
+                                    {adults + children === 0 ? "Add guests" : `${adults + children}`}
+                                </span>
+                            </div>
+                            <div
+                                className={`filters__options ${showLocation ? "" : "show"
+                                    }`}
+                            >
+                                <div >
+                                    <h2 >Adults</h2>
+                                    <span >Ages 13 or above</span>
+                                    <div >
+                                        <button
+                                            className="filters__button"
+                                            onClick={() =>
+                                                adults > 0 && setAdults((prevAdults) => prevAdults - 1)
+                                            }
+                                        >
+                                            -
+                                        </button>
+                                        <span>{adults}</span>
+                                        <button
+                                            className="filters__button"
+                                            onClick={() => setAdults((prevAdults) => prevAdults + 1)}
+                                        >
+                                            +
+                                        </button>
+                                    </div>
+                                </div>
+                                <div >
+                                    <h2 >Children</h2>
+                                    <span >Ages 2-12</span>
+                                    <div >
+                                        <button
+                                            className="filters__button"
+                                            onClick={() =>
+                                                children > 0 && setChildren((prevChildren) => prevChildren - 1)
+                                            }
+                                        >
+                                            -
+                                        </button>
+                                        <span>{children}</span>
+                                        <button
+                                            className="filters__button"
+                                            onClick={() => setChildren((prevChildren) => prevChildren + 1)}
+                                        >+</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div >
-                            <button onClick={() => setShowFilters(false)} ><AiOutlineSearch />  Search</button>
+                            <h2 >Edit your search</h2>
+                            <div >
+                                <button
+                                    className="filters__button"
+                                    onClick={() => setShowFilters(false)}
+                                >
+                                    <AiOutlineSearch /> Search
+                                </button>
+                            </div>
+                            <AiOutlineClose
+                                className="filters__close"
+                                onClick={() => setShowFilters(false)}
+                            />
                         </div>
-                        <AiOutlineClose onClick={() => setShowFilters(false)} />
-                    </div>
+
+
                 </div>
-            )}
-        </>
+            }
+        </div>
     )
 }
+
+
+
+
+        
+
+
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,14 @@ import { AiOutlineSearch } from "react-icons/ai"
 export default function Header({ setShowFilters, location, adults, children }) {
     const locationText = location ? `${location}` : "Add location"
     const guestsText = adults + children === 0 ? "Add guests" : `${adults + children}`
-
+    
     return (
         <header className="Header">
             {/* Icono */}
-            <img className="Logo" src="https://raw.githubusercontent.com/pipetboy2001/Windbnb/c7754f256d8cbf5a8da7a08f33ab5939b20b18a0/public/Logo.svg" />
+            <img className="Logo" src="https://raw.githubusercontent.com/pipetboy2001/Windbnb/c7754f256d8cbf5a8da7a08f33ab5939b20b18a0/public/Logo.svg" 
+                onClick={() => {
+                    window.location.href = '/'; 
+                }}/>
             {/* Buscador */}
             <div className="Header_search">
                 <h2 onClick={() => setShowFilters(true)} className="desktopHeader_search_location">{locationText}</h2>
